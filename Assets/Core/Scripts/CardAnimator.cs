f5fc894b2858b5e43a2b0874b078f49a92ef477d
RUBIUS\ChebanovDD <dmitriy.chebanov@rubius.com>
2021-02-19 19:08:15 +0700

    Added stack animation settings.

--- Assets/Core/Scripts/CardAnimator.cs
+++ Assets/Core/Scripts/CardAnimator.cs
@@ -7,12 +7,27 @@ namespace Core
 {
     public class CardAnimator : MonoBehaviour
     {
+        [Header("Stack Animation")]
+        [SerializeField] private float _duration = 0.4f;
+        [SerializeField] private Ease _easeType;
+
+        [Header("Activate Card Animation")]
         [SerializeField] private float _activateDuration = 0.4f;
         [SerializeField] private float _deactivateDuration = 0.4f;
         [SerializeField] private Ease _activateEaseType;
         [SerializeField] private Ease _deactivateEaseType;
         [SerializeField] private RectTransform _activeCardTargetPosition;
 
+        public Sequence SetCardTransform(ICard card, CardTransform cardTransform)
+        {
+            return DOTween.Sequence()
+                .Join(card.GameObject.transform.DOMove(cardTransform.Position, _duration))
+                .Join(card.GameObject.transform.DORotate(new Vector3(0, 0, -cardTransform.Angle), _duration))
+                .SetEase(_easeType).OnComplete(() =>
+                {
+                    card.SetTransform(cardTransform);
+                });
+        }
 
         public Sequence ActivateCard(ICard card)
         {
@@ -29,17 +44,6 @@ namespace Core
                 .Join(card.GameObject.transform.DOMoveY(card.StackPosition.y, _deactivateDuration))
                 .Join(card.GameObject.transform.DORotateQuaternion(card.StackRotation, _deactivateDuration))
                 .SetEase(_deactivateEaseType);
-        }
-
-        public Sequence SetCardTransform(ICard card, CardTransform cardTransform)
-        {
-            return DOTween.Sequence()
-                .Join(card.GameObject.transform.DOMove(cardTransform.Position, _deactivateDuration))
-                .Join(card.GameObject.transform.DORotate(new Vector3(0, 0, -cardTransform.Angle), _deactivateDuration))
-                .SetEase(_deactivateEaseType).OnComplete(() =>
-                {
-                    card.SetTransform(cardTransform);
-                });
         }
     }
 }
