ee63582a9a98a672c6b256e5a686a15e4a3e5f79
RUBIUS\ChebanovDD <dmitriy.chebanov@rubius.com>
2021-02-20 10:18:26 +0700

    Added ArcCardAnimator.

--- Assets/Core/Scripts/CardAnimator.cs
+++ Assets/Core/Scripts/CardAnimator.cs
@@ -5,45 +5,10 @@ using UnityEngine;
 
 namespace Core
 {
-    public class CardAnimator : MonoBehaviour
+    public abstract class CardAnimator : MonoBehaviour
     {
-        [Header("Stack Animation")]
-        [SerializeField] private float _duration = 0.4f;
-        [SerializeField] private Ease _easeType;
-
-        [Header("Activate Card Animation")]
-        [SerializeField] private float _activateDuration = 0.4f;
-        [SerializeField] private float _deactivateDuration = 0.4f;
-        [SerializeField] private Ease _activateEaseType;
-        [SerializeField] private Ease _deactivateEaseType;
-        [SerializeField] private RectTransform _activeCardTargetPosition;
-
-        public Sequence SetCardTransform(ICard card, CardTransform cardTransform)
-        {
-            return DOTween.Sequence()
-                .Join(card.GameObject.transform.DOMove(cardTransform.Position, _duration))
-                .Join(card.GameObject.transform.DORotate(new Vector3(0, 0, -cardTransform.Angle), _duration))
-                .SetEase(_easeType).OnComplete(() =>
-                {
-                    card.SetTransform(cardTransform);
-                });
-        }
-
-        public Sequence ActivateCard(ICard card)
-        {
-            return DOTween.Sequence()
-                .Join(card.GameObject.transform.DOMoveY(_activeCardTargetPosition.transform.position.y,
-                    _activateDuration))
-                .Join(card.GameObject.transform.DORotateQuaternion(Quaternion.identity, _activateDuration))
-                .SetEase(_activateEaseType);
-        }
-
-        public Sequence DeactivateCard(ICard card)
-        {
-            return DOTween.Sequence()
-                .Join(card.GameObject.transform.DOMoveY(card.StackPosition.y, _deactivateDuration))
-                .Join(card.GameObject.transform.DORotateQuaternion(card.StackRotation, _deactivateDuration))
-                .SetEase(_deactivateEaseType);
-        }
+        public abstract Sequence SetCardTransform(ICard card, CardTransform cardTransform);
+        public abstract Sequence ActivateCard(ICard card);
+        public abstract Sequence DeactivateCard(ICard card);
     }
 }
---
+++ Assets/ScreenSpaceImplementation/Scripts/ArcCardAnimator.cs
@@ -0,0 +1,47 @@
+﻿using Core;
+using Core.Interfaces;
+using Core.Models;
+using DG.Tweening;
+using UnityEngine;
+
+namespace ScreenSpaceImplementation.Scripts
+{
+    public class ArcCardAnimator : CardAnimator
+    {
+        [Header("Stack Animation")]
+        [SerializeField] private float _duration = 0.4f;
+        [SerializeField] private Ease _easeType;
+
+        [Header("Activate Card Animation")]
+        [SerializeField] private float _activateDuration = 0.4f;
+        [SerializeField] private float _deactivateDuration = 0.4f;
+        [SerializeField] private Ease _activateEaseType;
+        [SerializeField] private Ease _deactivateEaseType;
+        [SerializeField] private RectTransform _activeCardTargetPosition;
+
+        public override Sequence SetCardTransform(ICard card, CardTransform cardTransform)
+        {
+            return DOTween.Sequence()
+                .Join(card.GameObject.transform.DOMove(cardTransform.Position, _duration))
+                .Join(card.GameObject.transform.DORotate(new Vector3(0, 0, -cardTransform.Angle), _duration))
+                .SetEase(_easeType).OnComplete(() => { card.SetTransform(cardTransform); });
+        }
+
+        public override Sequence ActivateCard(ICard card)
+        {
+            return DOTween.Sequence()
+                .Join(card.GameObject.transform.DOMoveY(_activeCardTargetPosition.transform.position.y,
+                    _activateDuration))
+                .Join(card.GameObject.transform.DORotateQuaternion(Quaternion.identity, _activateDuration))
+                .SetEase(_activateEaseType);
+        }
+
+        public override Sequence DeactivateCard(ICard card)
+        {
+            return DOTween.Sequence()
+                .Join(card.GameObject.transform.DOMoveY(card.StackPosition.y, _deactivateDuration))
+                .Join(card.GameObject.transform.DORotateQuaternion(card.StackRotation, _deactivateDuration))
+                .SetEase(_deactivateEaseType);
+        }
+    }
+}
